8f6d8e2598c9122c14567b8727a6585fcd763ac5
iqwixn <aleksey.nizhnikov@gmail.com>
2023-07-26 18:21:20 -0700

    updated HTML, JS

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,28 @@
-var searchButton = document.getElementById('Search-Button');
+var cityTemp = document.getElementById('city-temp');
+var searchButton = document.getElementById('search-button');
 
-var cityName = ""
+
 
 
 
 function getApi() {
-    var requestUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + cityName + '&limit=5&appid=b4e27608152eaadfeaf81a63c8d579f6';
-  
-    var options = {
-       
-        }
-    };
+    var requestUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + searchButton + '&limit=5&appid=b4e27608152eaadfeaf81a63c8d579f6';
+
+    fetch(requestUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            for (var i = 0; i < data.length; i++) {
+                var listItem = document.createElement('button');
+                console.log(data[i].name);
+                listItem.textContent = data[i].name;
+                cityTemp.appendChild(listItem); 
+            }
+        });
+
+
+}
+
+searchButton.addEventListener('click', getApi);
